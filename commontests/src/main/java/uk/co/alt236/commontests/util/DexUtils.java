958ba5dd7ed0790e5ce2dd15e551eaf0901bc233
Alexandros Schillings <aschillings@gmail.com>
2015-04-02 12:59:14 +0100

    Deleted DexClassFilter

--- commontests/src/main/java/uk/co/alt236/commontests/util/DexUtils.java
+++ commontests/src/main/java/uk/co/alt236/commontests/util/DexUtils.java
@@ -1,33 +1,35 @@
-package uk.co.alt236.commontests.util.filter;
+package uk.co.alt236.commontests.util;
 
 import android.content.Context;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Enumeration;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import dalvik.system.DexFile;
 
 /**
- * Created by alexandros on 31/03/2015.
+ * Created by alexandros on 02/04/2015.
  */
-public class DexClassFilter extends SimpleClassFilter {
+public final class DexUtils {
 
-    public DexClassFilter(final Context context) {
-       final DexFile dexFile = getDexFile(context);
+    public static Collection<String> getClassNamesFromDex(final Context context){
+        final DexFile dexFile = getDexFile(context);
 
-        final List<String> methodResult = new ArrayList<>();
+        final Set<String> methodResult = new HashSet<>();
 
         for (Enumeration<String> iterator = dexFile.entries(); iterator.hasMoreElements(); ) {
             final String clazzName = iterator.nextElement();
             methodResult.add(clazzName);
         }
 
-        addClasses(methodResult);
+        return Collections.unmodifiableCollection(methodResult);
     }
 
-    private static DexFile getDexFile(final Context context) {
+    public static DexFile getDexFile(final Context context) {
         try {
             final String packageCodePath = context.getPackageCodePath();
             return new DexFile(packageCodePath);
